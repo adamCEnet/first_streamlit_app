37fa850f7246ddccea0a2e68b19407ced30ebdb8
adamCEnet <144076947+adamCEnet@users.noreply.github.com>
2023-09-06 09:32:12 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 
 #new section
-streamlit.header('Advice!')
+streamlit.header('Advice notes!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
